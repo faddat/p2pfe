7f516e81713a29d930677d45c3a9d14e16f5fece
Jacob Gadikian <jacobgadikian@gmail.com>
2020-09-26 22:42:36 +0700

    Added snippet from: https://github.com/ipfs/js-ipfs/tree/master/examples/ipfs-101 resulting in working file adds

--- src/main.js
+++ src/main.js
@@ -1,4 +1,32 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-
+import IPFS from "ipfs";
 createApp(App).mount("#app");
+
+
+async function main () {
+	const node = await IPFS.create()
+	const version = await node.version()
+
+	console.log('Version:', version.version)
+
+	const fileAdded = await node.add({
+		path: 'hello.txt',
+		content: 'Hello World 101'
+	})
+
+	console.log('Added file:', fileAdded.path, fileAdded.cid)
+
+	const chunks = []
+	for await (const chunk of node.cat(fileAdded.cid)) {
+		chunks.push(chunk)
+	}
+
+	console.log('Added file contents:', (chunks).toString())
+}
+
+main()
+
+
+
+
